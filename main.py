2f8ac9f61bda40d011910f735d9717dfe3689d6d
voriyaa <124890539+voriyaa@users.noreply.github.com>
2024-04-14 01:11:59 +0500

    Added Mario. remind end game if u win and solve maze

--- main.py
+++ main.py
@@ -8,5 +8,24 @@ screen = pygame.display.set_mode(MAZE_SIZE)
 pygame.display.set_caption('Maze Generator')
 icon = pygame.image.load('images/maze_icon.png')
 pygame.display.set_icon(icon)
+walk_right = [
+    pygame.image.load('images/walk_left/l1.png'),
+    pygame.image.load('images/walk_left/l2.png'),
+    pygame.image.load('images/walk_left/l3.png'),
+    pygame.image.load('images/walk_left/l4.png'),
+    pygame.image.load('images/walk_left/l5.png'),
+]
 
-generate_maze(screen, clock)
+walk_left = [
+    pygame.image.load('images/walk_right/r1.png'),
+    pygame.image.load('images/walk_right/r2.png'),
+    pygame.image.load('images/walk_right/r3.png'),
+    pygame.image.load('images/walk_right/r4.png'),
+    pygame.image.load('images/walk_right/r5.png'),
+]
+
+walk_up_down = [
+    pygame.image.load('images/walk_up_down/0.png')
+]
+
+generate_maze(screen, clock, walk_left, walk_right, walk_up_down)
--- src/MazeGenerator/maze_generator.py
+++ src/MazeGenerator/maze_generator.py
@@ -1,23 +1,32 @@
-import pygame
-from src.Constants.Constants import *
 from src.Cell.Cell import *
 from src.Cell.remove_walls import remove_walls
+from src.Wall.wall import *
 
 
-def generate_maze(screen, clock):
+def generate_maze(screen, clock, walk_right, walk_left, walk_up_down):
     screen.fill(GRID_COLOR)
-
     board = [Cell(col * CELL_SIZE, row * CELL_SIZE) for row in range(rows) for col in range(cols)]
     current_cell = board[0]
     stack = []
-
+    player_x = 15
+    player_y = 12
+    player_speed = 5
+    walking_left = False
+    walking_right = False
+    walking_up = False
+    walking_down = False
+    walk_index = 0
     running = True
     maze_generated = False
+    previous_player_x, previous_player_y = player_x, player_y
 
     while running:
+        if maze_generated:
+            board[cols * rows - 1].walls['right'] = False
         [cell.draw_cell(screen) for cell in board]
         current_cell.visit = True
-        current_cell.draw_current_cell(screen)
+        if not maze_generated:
+            current_cell.draw_current_cell(screen)
         next_cell = current_cell.get_neighbors(board)
         if next_cell:
             current_cell.visit = True
@@ -28,18 +37,68 @@ def generate_maze(screen, clock):
             current_cell = stack.pop()
         else:
             maze_generated = True
-        if maze_generated:
-            pass
+            if walking_left or walking_right or walking_up or walking_down:
+                if walking_left:
+                    player_x -= player_speed
+                    screen.blit(walk_left[walk_index % 5], (player_x, player_y))
+                    walk_index += 1
+                elif walking_right:
+                    player_x += player_speed
+                    screen.blit(walk_right[walk_index % 5], (player_x, player_y))
+                    walk_index += 1
+                elif walking_up:
+                    player_y -= player_speed
+                    screen.blit(walk_up_down[0], (player_x, player_y))
+                    walk_index += 1
+                elif walking_down:
+                    player_y += player_speed
+                    screen.blit(walk_up_down[0], (player_x, player_y))
+                    walk_index += 1
+            else:
+                screen.blit(walk_up_down[0], (player_x, player_y))
 
         pygame.display.flip()
+
+        wall_rects = create_wall_rects(board)
+
+        player_rect = pygame.Rect(player_x - 1, player_y - 1, 26, 37)
+        for wall_rect in wall_rects:
+            if player_rect.colliderect(wall_rect):
+                player_x, player_y = previous_player_x, previous_player_y
+                break
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
                 pygame.quit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    walking_left = True
+                    walking_right = False
+                    walking_up = False
+                    walking_down = False
+                    walk_index = 0
+                elif event.key == pygame.K_RIGHT:
+                    walking_right = True
+                    walking_left = False
+                    walking_up = False
+                    walking_down = False
+                    walk_index = 0
+                elif event.key == pygame.K_UP:
+                    walking_up = True
+                    walking_down = False
+                    walking_left = False
+                    walking_right = False
+                    walk_index = 0
+                elif event.key == pygame.K_DOWN:
+                    walking_down = True
+                    walking_up = False
+                    walking_left = False
+                    walking_right = False
+                    walk_index = 0
+        previous_player_x, previous_player_y = player_x, player_y
 
-        clock.tick(100)
-
-    if maze_generated:
-        print("Done")
-
+        if maze_generated:
+            clock.tick(10)
+        else:
+            clock.tick(200)
